b03413887b22d47cad94a3a503efa5a5da4ddd07
Анна Садовникова <el.kuwaitmail.ru@Annas-MacBook-Pro.local>
2022-08-27 21:55:18 +0300

    Dobavili gitignore

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+        
+        
+        Console.WriteLine("Vvedite 5ti znachnoe chislo");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int digitOnes = number%10;
+        int digitTens = number/10%10;
+        int digitThousands = number/1000%10;
+        int digitTenOfThousands = number/10000;
+        
+        if (digitOnes == digitTenOfThousands && digitTens == digitThousands)
+        {
+            Console.WriteLine("eto palindrom");
+        }
+        else 
+        {
+            Console.WriteLine("eto ne palindrom");
+        } 
