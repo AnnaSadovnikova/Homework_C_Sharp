e4156202c9ad345c76d2a14db9c293dd76af28d9
AnnaSadovnikova <108054191+AnnaSadovnikova@users.noreply.github.com>
2022-09-07 21:49:16 +0300

    Add files via upload

---
+++ Zadacha41.cs
@@ -0,0 +1,19 @@
+﻿void Zadacha41()
+{
+    //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    int m = 5;
+    int count = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine();
+        Console.Write($"Vvedite {i}-e chislo: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) count++;
+    }
+    Console.WriteLine("Kolichestvo chisel bolshe nulya:" + count);
+
+}
+
+Zadacha41();
